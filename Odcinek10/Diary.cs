b7f5d9fc8b8fc2f1b1388c2b9705b35e62a13b7c
krzysiekjunior <krzysiek.junior@gmail.com>
2019-02-09 21:26:30 +0100

    Zmienna typu prostego i referencyjnego

---
+++ Odcinek10/Diary.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Diary
+    {
+        public Diary()
+        {
+            ratings = new List<float>();
+        }
+
+        //Stan (zmienne - pola)
+        List<float> ratings;              //lista ocen
+
+
+        //Zachowania
+
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);            //metoda dodaje ocenę do listy ocen
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+            foreach (var rating in ratings)
+            {
+                sum += rating;
+            }
+            avg = sum / ratings.Count();    //oblicza średnią
+
+            return avg;
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();           //najwyższa ocena
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();           //najniższa ocena
+        }
+    }
+}
---
+++ Odcinek10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odcinek10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diary diary = new Diary();                  //zmienna typu referencyjnego nie przechowuje bezpośrednio obiektu,
+            diary.AddRating(3);                         //tylko przechowuje wskaźnik do obiektu
+            diary.AddRating(5);
+
+            Diary diary2 = diary;
+            diary2.AddRating(8);
+
+            Diary diary3 = diary2;
+            diary3.AddRating(1);
+
+            int liczba = 1;                     // zmienna typu prostego "liczba" przechwuje wartość(liczbę)
+        }
+    }
+}
